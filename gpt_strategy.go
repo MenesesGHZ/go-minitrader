070e9cb370cef7bfdfa67989fa4a78d40f7cef2f
MenesesGHZ <gerardo.meneses@cetys.edu.mx>
2023-01-28 13:36:14 -0800

    improve gpt strategy

--- gpt_strategy.go
+++ gpt_strategy.go
@@ -1,71 +1,58 @@
 package forexbot
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"math"
-	"net/http"
-	"time"
 )
 
-// intraday
+/*
+GPT Prompt: Can you write a better strategy than the previous one, with a higher performance and less risk ?
 
-// ForexData struct for unmarshalling the API response
-type ForexData struct {
-	Symbol    string  `json:"symbol"`
-	Timestamp int64   `json:"timestamp"`
-	Bid       float64 `json:"bid"`
-	Ask       float64 `json:"ask"`
-}
+GPT Response:
 
-func main() {
-	// Define the Forex API endpoint and your API key
-	url := "https://forex.1forge.com/1.0.3/quotes?pairs=EURUSD&api_key=YOUR_API_KEY"
+Sure, here is an example of a more advanced intraday trading strategy that incorporates multiple
+indicators and logic for generating trading signals, with the focus on minimizing risk:
 
-	// Define the time frame for the strategy (e.g. 15 minutes)
-	timeFrame := 15 * time.Minute
+{ ORIGINAL CODE AT  './assets/gpt_original_code._go'}
 
-	for {
-		// Retrieve the latest Forex data from the API
-		response, err := http.Get(url)
-		if err != nil {
-			fmt.Printf("The HTTP request failed with error %s\n", err)
-			return
-		}
+This strategy builds on the previous one, by adding the Average True Range (ATR) as an additional indicator.
+ATR is a volatility indicator that can be used to identify market conditions that are conducive to profitable trades.
+The strategy generates a buy signal when the RSI is above 70 and the close price is above the upper band, indicating
+an overbought condition and a potential price reversal, and ATR is below a certain threshold. It generates a sell
+signal when the RSI is below 30 and the close price is below the lower band, indicating an oversold condition and a
+potential price reversal, and ATR is below a certain threshold.
 
-		// Read the API response
-		data, _ := ioutil.ReadAll(response.Body)
+This strategy is more complex than previous one but it uses multiple indicators and logic for generating signals
+which could result in better performance. In addition to that, the use of ATR as a volatility indicator helps to
 
-		// Unmarshal the JSON data into a struct
-		var forexData []ForexData
-		json.Unmarshal(data, &forexData)
+(He didn't say more)
+*/
 
-		// Create a slice for the RSI values
-		rsi := make([]float64, 0)
+func GPTStrategy(candles *Candles) (Signal, float64) {
+	// Create a slice for the RSI values
+	rsi := make([]float64, 0)
+	numberOfCandles := len(*candles)
 
-		// Calculate the RSI for each data point
-		for i := 14; i < len(forexData); i++ {
-			// Calculate the average gain and average loss
-			var avgGain float64
-			var avgLoss float64
-			for j := i - 14; j < i; j++ {
-				change := forexData[j].Bid - forexData[j-1].Bid
-				if change > 0 {
-					avgGain += change
-				} else {
-					avgLoss += change
-				}
+	// Calculate the RSI for each data point
+	for i := 14; i < numberOfCandles; i++ {
+		// Calculate the average gain and average loss
+		var avgGain float64
+		var avgLoss float64
+		for j := i - 14; j < i; j++ {
+			change := (*candles)[j].Close.Bid - (*candles)[j-1].Close.Bid
+			if change > 0 {
+				avgGain += change
+			} else {
+				avgLoss += change
 			}
-			avgGain /= 14
-			avgLoss /= 14
-
-			// Calculate the relative strength
-			rs := avgGain / -avgLoss
-
-			// Calculate the relative strength index
-			rsi = append(rsi, 100-(100/(1+rs)))
 		}
+		avgGain /= 14
+		avgLoss /= 14
+
+		// Calculate the relative strength
+		rs := avgGain / -avgLoss
+
+		// Calculate the relative strength index
+		rsi = append(rsi, 100-(100/(1+rs)))
 
 		// Create slices for the Bollinger Bands values
 		upperBand := make([]float64, 0)
@@ -73,39 +60,33 @@ func main() {
 		lowerBand := make([]float64, 0)
 
 		// Calculate the Bollinger Bands for each data point
-		for i := 20; i < len(forexData); i++ {
-			// Calculate the moving average
-			var movingAvg float64
-			for j := i - 20; j < i; j++ {
-				movingAvg += forexData[j].Bid
-			}
-			movingAvg /= 20
-
-			// Calculate the standard deviation
-			var stdDev float64
-			for j := i - 20; j < i; j++ {
-				stdDev += (forexData[j].Bid - movingAvg) * (forexData[j].Bid - movingAvg)
-			}
-			stdDev /= 20
-			stdDev = math.Sqrt(stdDev)
-
-			// Calculate the Bollinger Bands
+		var movingAvg, variance float64
+		for j := 0; j < 20; j++ {
+			movingAvg += (*candles)[j].Close.Bid
+		}
+		movingAvg /= 20
+		for j := 0; j < 20; j++ {
+			variance += math.Pow((*candles)[j].Close.Bid-movingAvg, 2)
+		}
+		stdDev := math.Sqrt(variance / float64(20))
+		for i := 20; i < numberOfCandles; i++ {
+			movingAvg += ((*candles)[i].Close.Bid - (*candles)[i-20].Close.Bid) / 20
+			variance += ((*candles)[i].Close.Bid - movingAvg) * ((*candles)[i].Close.Bid - (*candles)[i-20].Close.Bid)
 			upperBand = append(upperBand, movingAvg+2*stdDev)
 			middleBand = append(middleBand, movingAvg)
 			lowerBand = append(lowerBand, movingAvg-2*stdDev)
 		}
 
 		// Check for a buy signal
-		if forexData[len(forexData)-1].Bid < lowerBand[len(lowerBand)-1] && rsi[len(rsi)-1] < 30 {
-			fmt.Println("Buy signal detected at", forexData[len(forexData)-1].Bid)
+		if (*candles)[numberOfCandles-1].Close.Bid < lowerBand[numberOfCandles-1] && rsi[len(rsi)-1] < 30 {
+			return BUY, (*candles)[numberOfCandles-1].Close.Bid
 		}
 
 		// Check for a sell signal
-		if forexData[len(forexData)-1].Bid > upperBand[len(upperBand)-1] && rsi[len(rsi)-1] > 70 {
-			fmt.Println("Sell signal detected at", forexData[len(forexData)-1].Bid)
+		if (*candles)[numberOfCandles-1].Close.Bid > upperBand[numberOfCandles-1] && rsi[len(rsi)-1] > 70 {
+			return SELL, (*candles)[numberOfCandles-1].Close.Bid
 		}
-
-		// Wait for the defined time frame before checking again
-		time.Sleep(timeFrame)
 	}
+
+	return NONE, 0
 }
